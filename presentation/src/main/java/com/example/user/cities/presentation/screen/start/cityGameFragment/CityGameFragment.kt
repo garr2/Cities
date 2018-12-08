fd395fed85ab25ef2392422572c560680df68b93
2312321301293 <123123123>
2018-12-08 19:11:16 +0300

    CityGameFragment add

--- presentation/src/main/java/com/example/user/cities/presentation/screen/start/cityGameFragment/CityGameFragment.kt
+++ presentation/src/main/java/com/example/user/cities/presentation/screen/start/cityGameFragment/CityGameFragment.kt
@@ -1,10 +1,20 @@
 package com.example.user.cities.presentation.screen.start.cityGameFragment
 
+import android.arch.lifecycle.ViewModelProviders
+import com.example.user.cities.databinding.CityGameFragmentBinding
 import com.example.user.cities.presentation.base.BaseMVVMFragment
 import com.example.user.cities.presentation.screen.start.MainRouter
 
 /**
  * Created by user on 08.12.2018.
  */
-class CityGameFragment: BaseMVVMFragment<CityGameViewModel,MainRouter,*> {
+class CityGameFragment: BaseMVVMFragment<CityGameViewModel,MainRouter,CityGameFragmentBinding>() {
+    override fun provideViewModel(): CityGameViewModel {
+        return ViewModelProviders.of(this)
+                .get(CityGameViewModel::class.java)
+    }
+
+    override fun provideLayoutId(): Int {
+       return R.layout.city_game_fragment
+    }
 }
